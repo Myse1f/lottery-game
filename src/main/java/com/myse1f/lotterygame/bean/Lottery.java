138868b2c96325179a11e251e05e11f718864113
Myse1f <3150101170@zju.edu.cn>
2019-12-10 16:20:03 +0800

    refactor structure

--- src/main/java/com/myse1f/lotterygame/bean/Lottery.java
+++ src/main/java/com/myse1f/lotterygame/bean/Lottery.java
@@ -1,14 +1,19 @@
-package com.myse1f.lotterygame.utils;
+package com.myse1f.lotterygame.bean;
 
 import com.myse1f.lotterygame.generated.LotteryGame;
+import com.myse1f.lotterygame.utils.Web3jConstants;
+import com.myse1f.lotterygame.utils.Web3jUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.tx.gas.StaticGasProvider;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
 
 @Component
 public class Lottery {
@@ -21,7 +26,11 @@ public class Lottery {
     public Lottery(Web3j web3j, Myself myself, @Value("${contract.address}") String contractAddress) throws IOException {
         this.web3j = web3j;
         this.contractAddress = contractAddress;
-        lotteryContract = LotteryGame.load(contractAddress, this.web3j, myself.getCredentials(), new DefaultGasProvider());
+        lotteryContract = LotteryGame.load(
+                contractAddress, this.web3j,
+                myself.getCredentials(),
+                new StaticGasProvider(Web3jConstants.GAS_PRICE, Web3jConstants.GAS_LIMIT_GREETER_TX)
+        );
         if (!lotteryContract.isValid()) {
             throw new RuntimeException("Contract invalid");
         }
@@ -29,6 +38,11 @@ public class Lottery {
 
     public LotteryGame getLotteryContract() {
         return lotteryContract;
+    }
+
+    public int getSettlementBlock() throws ExecutionException, InterruptedException {
+        BigInteger settlement = lotteryContract.getSettlementBlock().sendAsync().get();
+        return settlement.intValue();
     }
 
     public String draw() throws Exception {
--- src/main/java/com/myse1f/lotterygame/bean/Myself.java
+++ src/main/java/com/myse1f/lotterygame/bean/Myself.java
@@ -1,4 +1,4 @@
-package com.myse1f.lotterygame.utils;
+package com.myse1f.lotterygame.bean;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
