513eb4fbb64a8fdefeea345b7b2cf052fd0caffd
Myse1f <3150101170@zju.edu.cn>
2019-12-10 17:21:38 +0800

    update contract

--- src/main/resources/contracts/LotteryGame.sol
+++ src/main/resources/contracts/LotteryGame.sol
@@ -2,12 +2,13 @@ pragma solidity ^0.5.11;
 
 contract LotteryGame {
     address payable owner; // owner of this contract
-    address payable[] public players;
+    address payable[] players;
     uint BET = 0.1 ether;
-    uint8 public THRESHOLD = 11;
-    uint public settlementBlockNumber;
+    uint8 THRESHOLD = 11;
+    uint settlementBlockNumber;
+    address lastWinner;
 
-    event Play(address indexed player);
+    event Play(address indexed player, uint256 numOfPlayers);
     event Drawing(uint settlementBlockNumber);
     event Drawn(address indexed winner);
 
@@ -22,7 +23,7 @@ contract LotteryGame {
         require(players.length < THRESHOLD, "Waiting for lottery result");
 
         players.push(msg.sender); // record the players
-        emit Play(msg.sender);
+        emit Play(msg.sender, players.length);
 
         if (players.length == THRESHOLD) {
             // reach threshold, lock in the block hash in the future
@@ -40,9 +41,10 @@ contract LotteryGame {
         require(block.number < settlementBlockNumber + 256, "SettlementBlock is out of date!");
 
         uint8 winner = uint8(uint(keccak256(abi.encodePacked(blockhash(settlementBlockNumber), now)))) % THRESHOLD;
+        lastWinner = players[winner];
         players[winner].transfer((THRESHOLD-1) * BET); // 1 * BET for handling fee
 
-        emit Drawn(players[winner]);
+        emit Drawn(lastWinner);
 
         settlementBlockNumber = 0;
         delete players;
@@ -57,5 +59,21 @@ contract LotteryGame {
         }
 
         selfdestruct(owner);
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    function getSettlementBlock() public view returns (uint) {
+        return settlementBlockNumber;
+    }
+
+    function getThreshold() public view returns (uint8) {
+        return THRESHOLD;
+    }
+
+    function getLastWinner() public view returns (address) {
+        return lastWinner;
     }
 }
